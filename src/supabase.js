b4cb2f13d89972f6e8ef783718c411b64ebb65db
shiv <shivkumarraut@gmail.com>
2025-12-05 19:11:21 +0530

    new updates 1

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
+// src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://yukbqbqobpmmdvgugqeb.supabase.co", 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1a2JxYnFvYnBtbWR2Z3VncWViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MzI1MTMsImV4cCI6MjA4MDUwODUxM30.y7t8c206OwEkThZSQ0zauldI_S_kx83hqE4FAhA6QrA"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
